f0f187d5b1578b5f093142a10d8651a656e997ea
peter <peter@localhost.localdomain>
2023-11-04 10:13:45 +1100

    improved log tracing for tests

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use tracing_log::LogTracer;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
     LogTracer::init().expect("Failed to set logger");
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"));
