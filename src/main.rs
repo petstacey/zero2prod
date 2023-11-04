9daf4fb0f93434f206b1a5432a640c592c6ca170
petstacey <peter@peterstacey.com.au>
2023-11-04 16:29:58 +1100

    fixed dockerfile

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
 
     // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
@@ -22,7 +21,10 @@ async fn main() -> std::io::Result<()> {
     // server when it is bound to WSL2's localhost interface. As a workaround,
     // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
     // of the security implications when you expose the server on all interfaces.
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
