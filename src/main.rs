02a0b05fd5b0a576e4307931a4259f88466aacbb
petstacey <peter@peterstacey.com.au>
2023-11-04 14:44:10 +1100

    sqlx prepare for deployment

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-    LogTracer::init().expect("Failed to set logger");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
-    let configuration = get_configuration().expect("Failed to read configuration file");
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
+        PgPool::connect(configuration.database.connection_string().expose_secret())
             .await
-            .expect("Failed to connect to the database");
+            .expect("Failed to connect to Postgres.");
 
+    // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
+    // reasons. This binding may cause issues in some environments. For example,
+    // it causes connectivity issues running in WSL2, where you cannot reach the
+    // server when it is bound to WSL2's localhost interface. As a workaround,
+    // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
+    // of the security implications when you expose the server on all interfaces.
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(&address)?;
-
-    run(listener, connection_pool)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
